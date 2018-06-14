3cbde3ba3394faacc4711631dc0a529eea98f26c
R. Scout Kingery <rsj.kingery@gmail.com>
2018-06-13 17:12:15 -0700

    register lists app w/ django; use template, django test client in utest

--- lists/tests.py
+++ lists/tests.py
@@ -12,10 +12,5 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()                     # create request object
-        response = home_page(request)               # make request to specific view, return type HtmlResponse
-        html = response.content.decode('utf8')      # decode response to html
-
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do Lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
