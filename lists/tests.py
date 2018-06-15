067adfcdc29a0e90e90190772a8a007a037eec24
R. Scout Kingery <rsj.kingery@gmail.com>
2018-06-15 10:51:44 -0700

    add list model; item has model; make migrations

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -39,7 +39,9 @@ class ListViewTest(TestCase):
 
 
     def test_displays_all_items(self):
-        pass
+        list_ = List.objects.create()
+        Item.objects.create(text='thing1', list=list_)
+        Item.objects.create(text='thing1', list=list_)
 
 
 class NewListTest(TestCase):
@@ -56,16 +58,24 @@ class NewListTest(TestCase):
         self.assertRedirects(response, '/lists/this-is-the-list/')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()  # instantiate local List to which to add to each item, created in models.py
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first item, ever!'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second item!'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -73,4 +83,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first item, ever!')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The second item!')
+        self.assertEqual(second_saved_item.list, list_)
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['item_text'], list=list_)
         return redirect('/lists/this-is-the-list/')
 
     items = Item.objects.all()
@@ -18,5 +19,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/this-is-the-list/')
