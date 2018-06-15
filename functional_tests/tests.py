df09b410cd0faf22a5a860af12448ac0d37b1869
R. Scout Kingery <rsj.kingery@gmail.com>
2018-06-14 20:48:06 -0700

    test multiple lists w/ unique urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -19,7 +22,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
 
-    def test_start_list_and_retrieve(self):
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
+    def test_start_list_for_one_user(self):
 
         # Arvydas goes to the to-do list app web address
         self.browser.get(self.live_server_url)    # get http resource
@@ -41,27 +59,58 @@ class NewVisitorTest(LiveServerTestCase):
 
         # When he hits enter, the to-do list reloads and displays "1: dunk on Shaq"
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1: dunk on Shaq')
+        self.wait_for_row_in_list_table('1: dunk on Shaq')
 
         # there is still a text box inviting him to enter another item. He enters "take a towel to the face"
         input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            input_box.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
         input_box.send_keys('take a towel to the face')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # the page reloads and shows both items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')    # tr for table record?
-        self.check_for_row_in_list_table('1: dunk on Shaq')
-        self.check_for_row_in_list_table('2: take a towel to the face')
+        self.wait_for_row_in_list_table('1: dunk on Shaq')
+        self.wait_for_row_in_list_table('2: take a towel to the face')
 
-        # He sees a special url and some instructions about how it will link back to this list
-        self.fail("finish the test, ya dingus!")
+        # satisfied, arvydas goes back to sleep
 
-        # Arvudas follows that link and sees his to-do list
+
+    def test_start_list_multiple_users_with_urls(self):
+        
+        # Arvydas starts a new to-do list
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('dunk on Shaq')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: dunk on Shaq')
+
+        # he notices that the list has a unique url
+        arvydas_list_url = self.browser.current_url
+        self.assertRegex(arvydas_list_url, '/lists/.+')
+
+        # now a new user, Jeff, comes along to make a list
+        # he obviously is browsing in his own session...
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # jeff visits the home page, there is no sign of arvydas's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('dunk on Shaq', page_text)
+        self.assertNotIn('take a towel to the face', page_text)
+
+        # jeff enters his own items...
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('pick my nose')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: pick my nose')
+
+        # jeff gets his own url
+        jeff_list_url = self.browser.current_url
+        self.assertRegex(jeff_list_url, '/lists/.+')
+        self.assetNotEqual(jeff_list_url, arvydas_list_url)
+
+        #  again there is no trace of arvydas's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('dunk on Shaq', page_text)
+        self.assertNotIn('take a towel to the face', page_text)
+
+        # satisfied they both go back to sleep
